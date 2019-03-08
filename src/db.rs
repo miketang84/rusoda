22ac68946b5a5ed802d0b153e8ebe73df9977791
Mike Tang <daogangtang@gmail.com>
2019-03-08 15:22:28 +0800

    adapted to new rustorm nor2d2 branch.
    Signed-off-by: Mike Tang <daogangtang@gmail.com>

--- src/db.rs
+++ src/db.rs
@@ -1,5 +1,5 @@
 use std::env;
-use rustorm::{Pool, EntityManager};
+use rustorm::{DbManager, EntityManager};
 
 use redis::Client as redis_client;
 use redis::Connection as RedisConnection;
@@ -8,7 +8,7 @@ use redis::Connection as RedisConnection;
 
 pub fn get_db() -> EntityManager {
     let db_url = env::var("DBURL").expect("DBURL must be set");
-    let mut pool = Pool::new();
+    let mut pool = DbManager::new();
     let em = pool.em(&db_url).unwrap();
 
     em
