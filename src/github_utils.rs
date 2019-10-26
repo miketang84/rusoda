8aa00e226798d2505a0baa4a68b42876776d07c3
Mike Tang <daogangtang@gmail.com>
2019-10-26 15:49:09 +0800

    add another github oauth.
    Signed-off-by: Mike Tang <daogangtang@gmail.com>

--- src/github_utils.rs
+++ src/github_utils.rs
@@ -66,6 +66,52 @@ pub fn get_github_token(code: &str) -> Result<String, SapperError> {
     received
 }
 
+pub fn get_github_token2(code: &str) -> Result<String, SapperError> {
+    let _code = code.to_owned();
+    let (tx, rx) = mpsc::channel();
+    thread::spawn(move || {
+        let client = create_https_client();
+
+        let params = serde_urlencoded::to_string([
+            ("client_id", "a221029df754c826a6ba"),
+            ("client_secret", "1c1785e2eb050d6c55db0eb876f2012d83a93b6f"),
+            ("code", &_code[..]),
+            ("accept", "json"),
+        ]).unwrap();
+
+        let ret = client
+            .post("https://github.com/login/oauth/access_token")
+            .header(ContentType::form_url_encoded())
+            .body(&params)
+            .send()
+            .map_err(|e| SapperError::Custom(format!("hyper's io error: '{}'", e)))
+            .and_then(|mut response| {
+                let mut body = String::new();
+                response
+                    .read_to_string(&mut body)
+                    .map_err(|e| SapperError::Custom(format!("read body error: '{}'", e)))
+                    .map(|_| body)
+            })
+            .and_then(|ref body| {
+                #[derive(Deserialize)]
+                struct Inner {
+                    access_token: String,
+                }
+                serde_urlencoded::from_str::<Inner>(body)
+                    .map_err(|_| SapperError::Custom(String::from("No permission")))
+                    .map(|inner| inner.access_token)
+            });
+
+        tx.send(ret).unwrap();
+    });
+
+    let received: Result<String, SapperError> = rx.recv().unwrap();
+    println!("Got: {:?}", received);
+
+    received
+}
+
+
 pub fn get_github_user_info(raw_token: &str) -> Result<GithubUserInfo, SapperError> {
     let token = serde_urlencoded::to_string([("access_token", raw_token)]).unwrap();
 
@@ -121,4 +167,3 @@ pub fn get_github_user_info(raw_token: &str) -> Result<GithubUserInfo, SapperErr
 
     received
 }
-
